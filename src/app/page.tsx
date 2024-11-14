aff51c907e1b9d7464f32c3ec3d20c70e9cef3f3
Maria Eduarda Carmona <eduardacarmona@gmail.com>
2024-11-14 11:29:17 -0300

    Melhor svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-2 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="fixed inset-0 -z-10">
+      <div className="fixed inset-0 -z-10 bg-white">
         <Image
-          src="/assets/map_1966_divisao_municipal_rs.png"
+          src="/assets/map_1966_divisao_municipal_limpo.svg"
           alt="Mapa 1966"
           fill
           priority
