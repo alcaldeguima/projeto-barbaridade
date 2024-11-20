a21b7907438b4f6b72cef2dfb0ddece5047a7e3d
Maria Eduarda Carmona <eduardacarmona@gmail.com>
2024-11-20 18:59:27 -0300

    SVG outline rs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,17 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-    },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out',
+        'slide-in-left': 'slideInLeft 1s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+    }
+  },
   },
   plugins: [],
 };
